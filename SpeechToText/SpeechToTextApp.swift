12ed88c82d78c50fc5b0145806c8b91c28ff11d1
MinhQuan-Github <minhquan.5322@aol.com>
2023-03-26 00:37:33 +0700

    init source

---
+++ SpeechToText/SpeechToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextApp.swift
+//  SpeechToText
+//
+//  Created by Minh Quan on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpeechToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
